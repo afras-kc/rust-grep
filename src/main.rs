954bf71a56047284931319aec77881f7bdb573f8
Afras <afras.kcr@gmail.com>
2023-01-17 17:12:24 +0000

    added args, added file read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("{}\n{}", query, file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect_err("Unable to read file!");
+    println!("\nFile:\n\n{}", contents);
 }
