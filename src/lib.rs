03aa193721881209459ded878f29be3a7b1f1fc1
Afras <afras.kcr@gmail.com>
2023-01-28 16:46:36 +0000

    Added iterators to build fn and search fn

--- src/lib.rs
+++ src/lib.rs
@@ -54,14 +54,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build<T>(mut args: T) -> Result<Config, &'static str> where
+    T: Iterator<Item = String>
+    {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(args) => args,
+            None => return Err("No query argument received."),
+        };
 
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No file path received."),
+        };
+ 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
@@ -69,25 +76,18 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let lowercase_query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line
+                                .to_lowercase()
+                                .contains(query.to_lowercase().as_str()))
+        .collect()
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&lowercase_query) {
-            results.push(line);
-        }
-    }
-
-    results
 }
